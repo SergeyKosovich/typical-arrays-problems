a7c05b1a82b31cbb822d0fe3f28b9c762ec9ac4d
SergeyKosovich <kosovichsergey322@gmail.com>
2021-02-26 18:33:57 +0300

    init:start task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr){
+  let e;
+  if(typeof(arr)==='object'){ e=Math.min(...arr)}
+  return isFinite(e)? e: 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr){
+  let e;
+  if(typeof(arr)==='object'){ e=Math.max(...arr)}
+  return isFinite(e)? e: 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr){
+  let e;
+  if(typeof(arr)==='object'){ e=Math.max(...arr)}
+  return isFinite(e)? arr.reduce((first,item)=>first+item,0)/arr.length: 0;
 }
